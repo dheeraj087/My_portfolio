f813db68d0a81e11759d3fac1144484c03121d39
dheeraj087 <dwivedidheeraj087@gmail.com>
2025-11-14 18:06:07 +0530

    %msg%

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 import tailwindcss from '@tailwindcss/vite'
+import viteCompression from 'vite-plugin-compression'
 // https://vite.dev/config/ 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), viteCompression()],
+  // 🔁 यहाँ अपनी GitHub repo का नाम दो
+  optimizeDeps: {
+    include: ["react", "react-dom"]
+  }, 
+  
   base: '/My_portfolio/',
-  server:{
-    host:"0.0.0.0",
-    port:3000
+  server: {
+    host: "0.0.0.0",
+    port: 3000
   }
 })
 
